5c0889348213164e01ea6e5d79e96cc2f8579e18
Liangjie XIA <leejayxia@gmail.com>
2014-07-20 00:17:35 -0400

    blob upload handler

--- main_handler.py
+++ main_handler.py
@@ -22,9 +22,12 @@ import jinja2
 import logging
 import os
 import webapp2
+import urllib
 
 from google.appengine.api import memcache
 from google.appengine.api import urlfetch
+from google.appengine.ext import blobstore
+from google.appengine.ext.webapp import blobstore_handlers
 
 import httplib2
 from apiclient import errors
@@ -34,7 +37,6 @@ from oauth2client.appengine import StorageByKeyName
 
 from model import Credentials
 import util
-from google.appengine.ext import blobstore
 
 jinja_environment = jinja2.Environment(
     loader=jinja2.FileSystemLoader(os.path.dirname(__file__)))
@@ -355,10 +357,32 @@ class JournalHandler(webapp2.RequestHandler):
 class RequestUploadHandler(webapp2.RequestHandler):
   def get(self):
     upload_url = blobstore.create_upload_url('/journal')
-    self.response.out.write(upload_url)
+    if self.request.get("form"):
+      self.response.out.write('<html><body>')
+      self.response.out.write('<form action="%s" method="POST" enctype="multipart/form-data">' % upload_url)
+      self.response.out.write("""Upload File: <input type="file" name="file"><br> <input type="submit"
+          name="submit" value="Submit"> </form></body></html>""")
+    else:
+      self.response.out.write(upload_url)
+
+class UploadHandler(blobstore_handlers.BlobstoreUploadHandler):
+  def post(self):
+    upload_files = self.get_uploads('file')  # 'file' is file upload field in the form
+    blob_info = upload_files[0]
+    self.redirect('/serve/%s' % blob_info.key())
+
+class ServeHandler(blobstore_handlers.BlobstoreDownloadHandler):
+  def get(self, resource):
+    resource = str(urllib.unquote(resource))
+    blob_info = blobstore.BlobInfo.get(resource)
+    self.send_blob(blob_info)
+
+
 
 MAIN_ROUTES = [
     ('/', MainHandler),
     ('/journal', JournalHandler),
-    ('/request-upload', RequestUploadHandler)
+    ('/request-upload', RequestUploadHandler),
+    ('/upload', UploadHandler),
+    ('/serve/([^/]+)?', ServeHandler)
 ]
